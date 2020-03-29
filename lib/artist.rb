7bb19bcf96b4876dd28d86521a60a1a5c3926388
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-29 00:35:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+
+  @@songs = []
+  @@song_count = 0
+
+  attr_accessor :name, :song
+
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+
+    @songs << song
+    song.artist = self
+    @@songs << song
+    @@song_count += 1
+
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    add_song(song)
+    @songs << song
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
+© 2020 GitHub, Inc.
